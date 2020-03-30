17e7c1f28754b99b9dc2de4ec5de201a40fba3e0
Ante Hellgren <antehellgren@gmail.com>
2020-03-30 13:53:35 +0200

    task started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import SearchScreen from './components/Screens/SearchScreen';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div>
+      <SearchScreen/>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,6 @@ import React from 'react'
  function Navigation() {
     return (
         <div>
-            
         </div>
     )
 }
