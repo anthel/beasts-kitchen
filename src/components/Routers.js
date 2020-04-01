8deb6fd56d5ca40396d6048be8a801f25407b362
Ante <antehellgren@gmail.com>
2020-04-01 15:41:28 +0200

    fixed searchScreen

--- src/components/Routers.js
+++ src/components/Routers.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
 import Home from './Screens/HomeScreen';
-import Login from './Screens/Login';
+/* import Login from './Screens/Login'; */
 import Search from './Screens/SearchScreen';
+import Register from './Screens/Registration';
 
 const Routers = () => (
   <Switch>
     <Route exact path="/" component={Home} />
-    <Route path="/register" component={Login} />
+    <Route path="/register" component={Register} />
     <Route path="/search" component={Search} />
   </Switch>
 )
