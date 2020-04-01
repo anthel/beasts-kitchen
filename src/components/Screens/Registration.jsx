841c69edabfac1d2362ebe069fe2db84a0227dca
Ante <antehellgren@gmail.com>
2020-04-01 13:22:42 +0200

    multiple inputs

--- src/components/Screens/Registration.jsx
+++ src/components/Screens/Registration.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
 import Container from '@material-ui/core/Container';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import InputLabel from '@material-ui/core/InputLabel';
+/* import ArrowBackIcon from '@material-ui/icons/ArrowBack'; */
 
 
 
@@ -13,22 +15,58 @@ const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '15ch',
     },
   },
+  halfSize: {
+    width: '17.3ch',
+  },
+  fullSize: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '15ch',
+    },
+  }
 }));
 
 export default function Registration() {
 
+  const [firstname, setFirstname] = React.useState('');
+  /* const [lastname, setLastname] = React.useState('');
+  const [email, setEmail] = React.useState(''); */
+  /* const [password, setPassword] = React.useState(''); */
   const classes = useStyles();
+
+  const handleChange = (event) => {
+    setFirstname(event.target.value);
+  };
 
   return (
     <Container maxWidth='md'>
-      <ArrowBackIcon/>
+    {/*   <ArrowBackIcon/> */}
       <h1>Create Account</h1>
       <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Firstname" />
-        <TextField id="standard-basic" label="Lastname" />
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="register-firstname">Name</InputLabel>
+        <OutlinedInput id="register-firstname" 
+          value={firstname} 
+          onChange={handleChange} 
+          label="Name" 
+          className={classes.halfSize}
+          />
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel htmlFor="register-lastname">Name</InputLabel>
+        <OutlinedInput id="register-lastname" 
+          value={firstname} 
+          onChange={handleChange} 
+          label="Name" 
+          className={classes.halfSize}
+          />
+      </FormControl>
+      <FormControl variant="outlined" fullWidth>
+        <InputLabel htmlFor="register-email">Name</InputLabel>
+        <OutlinedInput id="register-email" value={firstname} onChange={handleChange} label="Name" />
+      </FormControl>
       </form>
     </Container>
   )
